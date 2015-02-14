5751647141c2a10483fd481d7c39efb5d4e39f21
hvozdulych <hvozdulych@gmail.com>
2015-02-14 11:37:47 +0200

    initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CentresOfCommutation::Application.config.secret_token = '461c982636f9a252c88fb5bd28bd5ebdd6a6e3334c091a3f540a522c8c0d06d12401b8f05a7c418bb4dedfb862ad1a02bbf3a72ea9e5b359d7fa5daf3e89d215'
